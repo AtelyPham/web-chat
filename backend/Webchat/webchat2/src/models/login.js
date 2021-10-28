2d076a057e79b5b4863d2dba7ce7ee89567b02e8
unknown <vanthang23032001@gmail.com>
2021-10-28 10:38:48 +0700

    Add Fe and Be

--- backend/Webchat/webchat2/src/models/login.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const Login = new mongoose.Schema({
-    username: String,
-    password: String,
-});
-
-module.exports = mongoose.model('login',Login);
--- backend/Webchat/webchat2/src/public/accesss/script.js
+++
@@ -1,8 +0,0 @@
-const formSubmit = document.querySelector('.submitEl');
-const inputVal = document.querySelector('.message-input');
-
-formSubmit.addEventListener('submit', e => {
-    if(inputVal.value){
-        inputVal.value = '';
-    }
-})
